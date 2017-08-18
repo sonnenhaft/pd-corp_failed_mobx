9e096f0037c7c34327d4c77694b4227e00b6ff63
Vladimir Shein <vladimir.sonnenhaft@gmail.com>
2017-08-18 22:13:24 +0200

    PDC-302 Hook for CSV parse for IE10, IE11

--- src/components/ImportAssetsPage/XlsUploadInput/XlsUploadInput.js
+++ src/components/ImportAssetsPage/XlsUploadInput/XlsUploadInput.js
@@ -1,21 +1,32 @@
 import React from 'react'
-import { compose, withHandlers, withState } from 'recompose'
-import { Button } from 'react-toolbox'
+import {compose, withHandlers, withState} from 'recompose'
+import {Button} from 'react-toolbox'
 import './XlsUploadInput.css'
 import assets from 'mobx/Assets.store'
-import { withRouter } from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 
-const isIE = () => {
-  const isIE11 = !!window.MSInputMethodContext && !!window.document.documentMode
-  const isIE10 = window.navigator.userAgent.toString().toLowerCase().indexOf('trident/6') > -1
-  return isIE11 || isIE10
+// ie10-11 polyfill
+if (!FileReader.prototype.readAsBinaryString) {
+  FileReader.prototype.readAsBinaryString = function (blob) {
+    const reader = new FileReader()
+    reader.onload = () => {
+      let binaryString = ''
+      const bytes = new Uint8Array(reader.result)
+      const length = bytes.byteLength
+      for (let i = 0; i < length; i++) {
+        binaryString += String.fromCharCode(bytes[i])
+      }
+      this.onload({target: {result: binaryString}})
+    }
+    reader.readAsArrayBuffer(blob)
+  }
 }
 
 const XlsUploadInput = props => {
-  const { proxyClick, onFilesSelected, setInputRef, children, className } = props
-  return <Button raised onClick={ proxyClick } { ...{ className } }>
-    <input ref={ setInputRef } type="file"
-           onChange={ onFilesSelected } style={ { display: 'none' } }/>
+  const {proxyClick, onFilesSelected, setInputRef, children, className} = props
+  return <Button raised onClick={proxyClick} {...{className}}>
+    <input ref={setInputRef} type="file"
+           onChange={onFilesSelected} style={{display: 'none'}}/>
     {children}
   </Button>
 }
@@ -24,27 +35,19 @@ export default compose(
   withRouter,
   withState('inputRef', 'setInputRef', null),
   withHandlers({
-    proxyClick: ({ inputRef, history }) => () => {
-      if ( isIE() ) {
-        const allowNext = window.confirm('Table upload feature is not working in IE yet. Press ok to go with stub data.')
-        if ( allowNext ) {
-          assets.setSheetToImport([{
-            ' TODO(vlad) remove IE stub data: ': '7/21/09',
-            ' Add hook for IE ': 'MONITORS: BEDSIDE: RESP: EXHALED CO2'
-          }])
-          history.push('/assets/import')
-        }
-      } else {
-        inputRef.click()
-      }
-
+    proxyClick: ({inputRef}) => () => {
+      // assets.setSheetToImport([{
+      //   ' stub field 1: ': '7/21/09',
+      //   ' stub field 2 ': 'MONITORS: BEDSIDE: RESP: EXHALED CO2'
+      // }]);
+      inputRef.click()
     },
-    onFilesSelected: ({ history }) => ({ target }) => {
+    onFilesSelected: ({history}) => ({target}) => {
       const reader = new FileReader()
 
       reader.onload = e => {
         const data = e.target.result
-        const workbook = window.XLSX.read(data, { type: 'binary' })
+        const workbook = window.XLSX.read(data, {type: 'binary'})
         console.log(workbook)
         const sheetData = workbook.Sheets[workbook.SheetNames[0]]
         assets.setSheetToImport(window.XLSX.utils.sheet_to_json(sheetData))
