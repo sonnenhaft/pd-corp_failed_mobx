a1c829375a99c758718c2b49a9747320bd8f6e28
Vladimir Shein <vladimir.sonnenhaft@gmail.com>
2017-09-05 14:03:39 +0300

    added mark highlight

--- src/components/AssetsPage/SearchInputs/AssetsAutocomplete.js
+++ src/components/AssetsPage/SearchInputs/AssetsAutocomplete.js
@@ -7,13 +7,15 @@ import { assets } from 'mobx-stores'
 import Autocomplete from 'react-toolbox/lib/autocomplete'
 import { debounce } from 'lodash'
 import cn from 'classnames'
+import Mark from 'mark.js/dist/mark.es6'
 
-const AssetsAutocomplete = ({ source, onFocus, localValue: value, onQueryChange, loading, label, onValueChange: onChange }) => {
-  return <div styleName={ cn('autocomplete-wrapper', { loading }) }>
+const AssetsAutocomplete = ({ source, onFocus, setRef, localValue: value, onQueryChange, loading, label, onValueChange: onChange }) => {
+  return <div styleName={ cn('autocomplete-wrapper', { loading }) } ref={ setRef }>
     <Autocomplete
       { ...{ label, source, onFocus, onChange, onQueryChange, value } }
       direction="down"
       selectedPosition="above"
+      className="highlitable-autocomplete"
       suggestionMatch="anywhere"
       multiple={ false }
       allowCreate={ true }/>
@@ -25,6 +27,7 @@ const update = debounce(action => action(), 500)
 
 export default compose(
   withState('source', 'setSource', []),
+  withState('reference', 'setRef', null),
   withState('loading', 'setLoading', false),
   withState('localValue', 'setLocalValue', ({ value }) => value),
   withPropsOnChange([], ({ setSource, setLoading, field }) => ({
@@ -36,12 +39,16 @@ export default compose(
       })
     }
   })),
-  withPropsOnChange([], ({ onChange, setLocalValue, loadValues }) => ({
+  withPropsOnChange(['reference'], ({ onChange, setLocalValue, loadValues, reference }) => ({
     onValueChange(value) {
       setLocalValue(value)
       onChange(value)
     },
     onQueryChange(value){
+      const instance = new Mark(reference.querySelectorAll('li'))
+      instance.unmark()
+      instance.mark(value, { caseSensitive: false })
+
       setLocalValue(value)
       update(() => {
         onChange(value)
