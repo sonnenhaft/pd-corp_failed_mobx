f092e6c3f92f37d10f7c2f91e789424fdaf79630
Vladimir Shein <vladimir.sonnenhaft@gmail.com>
2017-09-20 01:15:53 +0200

    added trim to all text inputs

--- src/common/TextInput/TextInput.js
+++ src/common/TextInput/TextInput.js
@@ -1,52 +1,60 @@
 import React from 'react'
 import P from 'prop-types'
-import { compose, setPropTypes, withState } from 'recompose'
-import Input from 'react-toolbox/lib/input'
+import { branch, compose, mapProps, renderComponent, setPropTypes, withPropsOnChange, withState } from 'recompose'
 import cn from 'classnames'
+import Input from 'react-toolbox/lib/input'
 
 import './TextInput.css'
 
-export const TextInput = ({ errorText, onEnterPressed, className, ...props }) => {
-  const onKeyPress = onEnterPressed && (e => e.key === 'Enter' && onEnterPressed())
-  let { multiline, disabled, value, ...leftProps } = props
-  if ( multiline && disabled ) {
-    return <DisabledTextArea { ...{ ...leftProps, onKeyPress, value } } className={ className }/>
-  } else {
-    if ( disabled && !props.value ) {
-      value = ' '
-    }
-    return <section className={ className }
-                    styleName={ cn(className, { disabledInput: disabled }) }>
-      <Input type="text" { ...{ ...leftProps, onKeyPress, value } }/>
-      {disabled && <span styleName="wrappable-value greyed-input">{value}</span>}
-      {errorText && <div><br/><br/></div>}
-    </section>
-  }
-}
+const WrappedInput = (({ errorText, className, disabledInput, ...props }) => <section
+  styleName={ cn({ disabledInput }) }
+  className={ className }>
+  <Input type="text" { ...props }/>
+  {disabledInput && <span styleName="wrappable-value greyed-input">
+        {props.value}
+  </span>}
+  {errorText && <div><br/><br/></div>}
+</section>)
 
-const MAX_LEN = 250
-const DisabledTextAreaDummy = props => {
-  const { className, setExpanded, expanded, multiline, value = '', disabled, label } = props
-  const collsapsible = multiline && value.length > MAX_LEN && disabled
+const MAX_TEXT_AREA_LENGTH = 250
+const DisabledMultilineInput = compose(
+  withState('expanded', 'setExpanded', false)
+)(({ className, setExpanded, expanded, multiline, value, disabled, label }) => {
+  const collapsible = multiline && value.length > MAX_TEXT_AREA_LENGTH && disabled
 
   return <section styleName="content" className={ className }>
     <label styleName="label">{label}</label>
-    {collsapsible && <div styleName="wrappable-value">
-      {expanded ? value : `${ value.slice(0, MAX_LEN) }...`}
+    {collapsible && <div styleName="wrappable-value">
+      {expanded ? value : `${ value.slice(0, MAX_TEXT_AREA_LENGTH) }...`}
       <a onClick={ () => setExpanded(!expanded) } styleName="expandable-button">
         {expanded ? 'Collapse' : 'Expand'}
       </a>
     </div>}
-    {!collsapsible && <div styleName="wrappable-value">{value}</div>}
+    {!collapsible && <div styleName="wrappable-value">{value}</div>}
   </section>
-}
+})
 
-const DisabledTextArea = compose(
+export const TextInput = compose(
   setPropTypes({
     multiline: P.bool,
     value: P.string,
+    className: P.string,
     disabled: P.bool,
     label: P.string
   }),
-  withState('expanded', 'setExpanded', false)
-)(DisabledTextAreaDummy)
+  withPropsOnChange(['onEnterPressed'], ({ onEnterPressed }) => ({
+    onKeyPress(e) { onEnterPressed && e.key === 'Enter' && onEnterPressed()}
+  })),
+  withPropsOnChange(['value', 'onChange', 'disabled'], ({ value, onChange, disabled }) => ({
+    onBlur() { typeof value === 'string' && onChange(value.trim())},
+    value: (disabled && !value) ? ' ' : (value || ''),
+    disabledInput: disabled && value
+  })),
+  // eslint-disable-next-line no-unused-vars
+  mapProps(({ onEnterPressed, ...props }) => props),
+  branch(
+    ({ multiline, disabled }) => multiline && disabled,
+    renderComponent(DisabledMultilineInput),
+    renderComponent(WrappedInput)
+  )
+)(() => null)
